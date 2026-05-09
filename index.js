6c9c0a38674524b7e168eaca762022cb8701c782
Farhad Yousefe <farhadyousefe@gmail.com>
2026-05-09 14:50:07 +0430

    add innerHTML to crypto selector to render in DOM

--- index.js
+++ index.js
@@ -39,10 +39,33 @@ const renderBackgroundImg = async () => {
   } catch (err) {
     console.log("Faild to fetch background image", err);
     const defaultUrl =
-      "https://images.unsplash.com/photo-1501785888041-af3ef285b470?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NzgyNDUzMDV8&ixlib=rb-4.1.0&q=85";
+      "https://images.unsplash.com/photo-1501785888041-af3ef2https://images.unsplash.com/photo-1470813740244-df37b8c1edcb?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NzgzMjA3ODN8&ixlib=rb-4.1.0&q=8585b470?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NzgyNDUzMDV8&ixlib=rb-4.1.0&q=85";
     document.body.style.backgroundImage = `url(${defaultUrl})`;
     // Report the error to some kind of service for diagnosis
   }
 };
 
+const renderCurrencyData = async () => {
+  try {
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!res.ok) {
+      throw new Error(`HTTP Error! status:${res.status}`);
+    }
+    const data = await res.json();
+    console.log(data);
+    document.getElementById("crypto-top").innerHTML = `
+          <img src=${data.image.small} />
+          <span>${data.name}</span>
+          `;
+  } catch (err) {
+    console.log("Faild to fetch background image", err);
+  }
+};
+
 renderBackgroundImg();
+renderCurrencyData();
